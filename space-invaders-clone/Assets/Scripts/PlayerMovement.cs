d8e81df07190d63630ee5b2f715510f78840e6e2
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-16 17:30:50 +0200

    Adding Player Input
    Adding Input to Controller Main character

---
+++ space-invaders-clone/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector2 direction = new Vector2(h, v);
+
+        rigidbody.velocity = direction.normalized * speed;
+    }
+}
