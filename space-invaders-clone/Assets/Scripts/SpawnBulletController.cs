a0ba6ab0bbe8e2673ecf682d291150b509596980
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-17 21:41:04 +0200

    Add BulletSpawnScript
    Adding new Script to Spawn Bullet for monster and player

---
+++ space-invaders-clone/Assets/Scripts/SpawnBulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBulletController : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float interval = 1f;
+    public float statingInvoke = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Method Invoke is called after startingInvoke and then every Interval
+        InvokeRepeating("ShootBullet", statingInvoke, interval);
+    }
+
+    void ShootBullet()
+    {
+
+        //New Bullet is Instantiate everytime ShootBullet Method is called
+        GameObject g = Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
