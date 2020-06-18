ba320f9f4735da731397f83479205d8cbeb4db73
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-18 23:29:32 +0200

    Add Collision detection
    Add script to destroy object if hit border

---
+++ space-invaders-clone/Assets/Scripts/BorderCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        // if the gamobject has tag player and bulletplayer will not be affected from destroy
+        if(collision.gameObject.tag != "Player" && collision.gameObject.tag != "BulletPlayer")
+        {
+
+            // Destroy the gemObject which is not attached to the script.
+            Destroy(collision.gameObject);
+        }
+    }
+}
