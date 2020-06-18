f3a16c5d0f901c9f02da55d3b7f15293f82e103d
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-18 23:04:22 +0200

    Comment script

--- space-invaders-clone/Assets/Scripts/MoveToPlayer.cs
+++ space-invaders-clone/Assets/Scripts/MoveToPlayer.cs
@@ -10,11 +10,18 @@ public class MoveToPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        // Find Gamobject by Tag
         GameObject gameObject = GameObject.FindWithTag("Player");
 
+        // Get the distance between object1 to object2 and nomalized the distance between them (a² + b² = c²)
         Vector3 v3 = (gameObject.transform.position - transform.position).normalized;
+
+        // Create new Vector 2d to get the distance between to object in 2D
         Vector2 v2 = new Vector2(v3.x, v3.y);
 
+        // Move Rigidbody from the gameobject which this script is attached to. 
+        // The velocity from this from ridgidbody2d is create by multiply the speed with the vector2 (v = a · t + v)
         GetComponent<Rigidbody2D>().velocity = v2 * speed;
 
     }
