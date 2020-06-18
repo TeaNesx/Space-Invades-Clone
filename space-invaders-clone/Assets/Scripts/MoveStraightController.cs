b6918ffc10f9c8d1dd0b5f4b52b3800ebfbee77d
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-18 22:35:54 +0200

    Add Bullet behaviour
    Create 2 scripts. 1 Script move object down, 2nd scripts move object to player

---
+++ space-invaders-clone/Assets/Scripts/MoveStraightController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStraightController : MonoBehaviour
+{
+
+    Rigidbody2D rigidbody2D;
+    public Vector2 vector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2D.velocity = vector;
+    }
+}
