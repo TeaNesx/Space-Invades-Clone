aa7bd9f3eb5db634d1ef3d84a9b5dfc3cc5aa0a1
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-24 23:45:25 +0200

    Adding Monster Bullet collision

--- space-invaders-clone/Assets/Scripts/CollisionMonsterBulletController.cs
+++ space-invaders-clone/Assets/Scripts/CollisionMonsterBulletController.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionMonsterBulletController : MonoBehaviour
+public class CollisionPlayerController : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Monster" || collision.gameObject.tag == "Bullet")
+        if (collision.gameObject.tag == "Monster" || collision.gameObject.tag == "Bullet")
         {
             // 1. collider ist das object, mit dem collidiert wurde
             // 2. collider von dem object selbst, welches das Skript besitzt
@@ -18,7 +18,7 @@ public class CollisionMonsterBulletController : MonoBehaviour
         {
             Destroy(gameObject);
 
-            if(collision.gameObject.tag != "Wall")
+            if (collision.gameObject.tag != "Wall")
             {
                 Destroy(collision.gameObject);
             }
--- space-invaders-clone/Assets/Scripts/MoveToPlayer.cs
+++ space-invaders-clone/Assets/Scripts/MoveToPlayer.cs
@@ -14,15 +14,20 @@ public class MoveToPlayer : MonoBehaviour
         // Find Gamobject by Tag
         GameObject gameObject = GameObject.FindWithTag("Player");
 
-        // Get the distance between object1 to object2 and nomalized the distance between them (a² + b² = c²)
-        Vector3 v3 = (gameObject.transform.position - transform.position).normalized;
+        if(gameObject != null)
+        {
 
-        // Create new Vector 2d to get the distance between to object in 2D
-        Vector2 v2 = new Vector2(v3.x, v3.y);
+            // Get the distance between object1 to object2 and nomalized the distance between them (a² + b² = c²)
+            Vector3 v3 = (gameObject.transform.position - transform.position).normalized;
 
-        // Move Rigidbody from the gameobject which this script is attached to. 
-        // The velocity from this from ridgidbody2d is create by multiply the speed with the vector2 (v = a · t + v)
-        GetComponent<Rigidbody2D>().velocity = v2 * speed;
+            // Create new Vector 2d to get the distance between to object in 2D
+            Vector2 v2 = new Vector2(v3.x, v3.y);
+
+            // Move Rigidbody from the gameobject which this script is attached to. 
+            // The velocity from this from ridgidbody2d is create by multiply the speed with the vector2 (v = a · t + v)
+            GetComponent<Rigidbody2D>().velocity = v2 * speed;
+
+        }
 
     }
 }
