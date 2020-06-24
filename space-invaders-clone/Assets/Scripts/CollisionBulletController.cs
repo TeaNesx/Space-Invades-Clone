2a7aaa1cac6a90c058390069096bcd3447df3fd0
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2020-06-24 23:31:13 +0200

    Add Bullet Collision and destroy

---
+++ space-invaders-clone/Assets/Scripts/CollisionBulletController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBulletController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Bullet")
+        {
+            // 1. collider ist das object, mit dem collidiert wurde
+            // 2. collider von dem object selbst, welches das Skript besitzt
+
+            Physics2D.IgnoreCollision(collision.gameObject.transform.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        else
+        {
+            Destroy(gameObject);
+
+            if(collision.gameObject.tag != "Wall")
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
